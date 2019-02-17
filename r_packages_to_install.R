1103782e9d983ed6d9b21242936d520ecc5f8712
williamcioffi <william.cioffi@gmail.com>
2019-02-16 19:32:15 -0500

    add ij new pack

--- r_packages_to_install.R
+++ r_packages_to_install.R
@@ -41,5 +41,7 @@ install.packages(cran_packages)
 # i guess roxygen2 is on cran now?
 # devtools::install_github("klutometis/roxygen")
 devtools::install_github("jalvesaq/colorout")
-devtools::install_github("ianjonsen/ssmTMB")
+# devtools::install_github("ianjonsen/ssmTMB") # abandoned i think?
+
+devtools::install_github("ianjonsen/foieGras")
 
